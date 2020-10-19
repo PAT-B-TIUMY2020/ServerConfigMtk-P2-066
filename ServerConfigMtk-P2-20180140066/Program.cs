697c0ecbb8071990dbc26680eaf78f2173580d66
aureladnan <aurelpopeye@gmail.com>
2020-10-19 20:09:49 +0700

    Add first commit

---
+++ ServerConfigMtk-P2-20180140066/Program.cs
@@ -0,0 +1,32 @@
+﻿using ServiceMtk_P1_20180140066;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigMtk_P2_20180140066
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch(Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
